b010e5dc8b8655895475ef4d219640c40d283254
MukitSany <mukit.sany288@gmail.com>
2026-03-05 00:32:13 +0600

    API integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import './App.css'
 import Navber from './navber'
 import Banner from './banner'
 import Tickets from './customer-tickets'
+import { Suspense } from 'react'
+
+const fetchTickets = async () => {
+  const res = await fetch("tickets.json")
+  return res.json()
+}
 
 function App() {
+  const ticketPromise = fetchTickets()
   return (
     <>
       <Navber></Navber>
       <Banner></Banner>
-      <Tickets></Tickets>
+      <Suspense fallback={<progress className="progress w-56"></progress>}>
+        <Tickets ticketPromise={ticketPromise}></Tickets>
+      </Suspense>
     </>
   )
 }
--- src/customer-tickets.jsx
+++ src/customer-tickets.jsx
@@ -1,52 +1,62 @@
+import { use } from "react"
 
 
 
 
-export default function Tickets() {
+export default function Tickets({ticketPromise}) {
+
+    const ticketInfprmation = use(ticketPromise)
+    console.log(ticketInfprmation);
     return (
         <div className="max-w-450 mx-auto">
+
+            
+
             <div>
                 <h1 className="font-extrabold text-3xl">Customer Tickets</h1>
-                <div>
+                <div className="">
+                    
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        {
+                ticketInfprmation.map(ticket=><div>
                     <div className="bg-white rounded-xl shadow-md p-6 w-full max-w-3xl">
         
-        {/* Top Section */}
+        
         <div className="flex items-start justify-between">
           
           <div>
             <h2 className="text-xl font-semibold text-gray-800">
-              Login Issues - Can't Access Account
+              {ticket.title}
             </h2>
 
             <p className="text-gray-500 mt-2">
-              Customer is unable to log in to their account. They've tried
-              resetting their password multiple times but still...
+              {ticket.description}
             </p>
           </div>
 
-          {/* Status Badge */}
+          
           <span className="flex items-center gap-2 bg-green-100 text-green-700 
           px-4 py-1 rounded-full text-sm font-medium">
             <span className="w-3 h-3 bg-green-500 rounded-full"></span>
-            Open
+            {ticket.status}
           </span>
         </div>
 
-        {/* Bottom Section */}
+        
         <div className="flex items-center justify-between mt-6 text-sm text-gray-500">
           
           <div className="flex items-center gap-4">
-            <span className="text-gray-600">#1001</span>
+            <span className="text-gray-600">{ticket.id}</span>
             <span className="text-red-500 font-semibold">
-              HIGH PRIORITY
+              {ticket.priority}
             </span>
           </div>
 
           <div className="flex items-center gap-6">
-            <span>John Smith</span>
+            <span>{ticket.customer}</span>
 
             <div className="flex items-center gap-2">
-              {/* Calendar Icon */}
+              
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-5 h-5 text-gray-400"
@@ -62,13 +72,25 @@ export default function Tickets() {
                 />
               </svg>
 
-              <span>1/15/2024</span>
+              <span>{ticket.createdAt}</span>
             </div>
           </div>
         </div>
 
       </div>
+                </div>)
+            }
+                    </div>
+
+                {/* <div className="p-12">
+                        <h1 className="font-extrabold">sdjkbvsdhbv</h1>
+                    </div> */}
+
+
+
                 </div>
+
+                
             </div>
         </div>
     )
